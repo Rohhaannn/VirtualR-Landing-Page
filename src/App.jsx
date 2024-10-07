12150e9eb3c79cf7e67c8e70229c1f8c1b3a2891
Rohan <shingaderohan96@gmail.com>
2024-10-07 13:36:31 +0530

    Features section done & rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero/>
+        <Features/>
       </div>
     </>
   )
